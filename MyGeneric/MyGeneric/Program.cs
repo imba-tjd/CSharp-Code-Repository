573e10506da39faaa567bf458f8f5ee82b3fb3c4
谭九鼎 <109224573@qq.com>
2017-05-13 14:06:41 +0800

    更新实例和栈

--- MyGeneric/MyGeneric/Program.cs
+++ MyGeneric/MyGeneric/Program.cs
@@ -16,13 +16,18 @@ namespace MyGeneric
             a.Push(2);
             a.Push(1);
             b.Add(1);
-            b.Add(1);
+            //b.Add(1);
             b.Add(6);
             b.Add(3);
             Console.WriteLine(a.Pop());
             Console.WriteLine(a.Pop());
             Console.WriteLine(a.Pop());
             Console.WriteLine(b[2]);
+            Console.WriteLine(b.Count);
+            b.Clear();
+            Console.WriteLine(b.Count);
+            int[] f = new int[0];
+            Console.WriteLine(f.Length);
             Stack<int> d;
             d = null;
             //Console.ReadKey();
@@ -45,12 +50,9 @@ namespace MyGeneric
             stack = new T[capacity];
         }
 
-        public int Capacity { get; private set; }
-        public int Count { get => top + 1; }
-        public void Clear()
-        {
-            top = -1;
-        }
+        public int Capacity { get; private set; }// 最大容量
+        public int Count { get => top + 1; }// 当前个数
+        public void Clear() => top = -1;
         public T Peek()
         {
             if (top == -1)
@@ -70,7 +72,7 @@ namespace MyGeneric
             stack[++top] = e;
         }
     }
-    class MySortedSet<T> where T : IComparable
+    class MySortedSet<T> where T : IComparable// 有序不重复集合
     {
         T[] mySet = new T[0];
         public MySortedSet(int size = 256)
@@ -82,34 +84,33 @@ namespace MyGeneric
             MaxSize = size;
         }
 
-
         public int Count { get => mySet.Length; }
         public int MaxSize { get; private set; }
         public T this[int index] { get => mySet[index]; }
+
         public void Add(T e)
         {
             if (mySet.Length == MaxSize)
                 throw new Exception("集合已满！");
-            if (mySet.Length == 0)
-            {
-                mySet = new T[1];
-                mySet[0] = e;
-                return;
-            }
+            //if (mySet.Length == 0)
+            //{
+            //    mySet = new T[1];
+            //    mySet[0] = e;
+            //    return;
+            //}
             for (int i = 0; i < mySet.Length; i++)
                 if (mySet[i].Equals(e))
-                    throw new Exception("给定元素已经存在！");
+                    throw new Exception("给定的元素已经存在！");
             T[] tempSet = new T[mySet.Length + 1];
-            tempSet[mySet.Length] = e;
-            for (int i = 0; i < mySet.Length; i++)
+            tempSet[mySet.Length] = e;// 直接把元素添加到最后一个
+            for (int i = 0,j=0; i < mySet.Length; i++)
             {
-                int j = 0;
                 if (e.CompareTo(mySet[i]) < 0)
                 {
                     tempSet[i] = e;
-                    j = 1;
+                    j = 1;//成功插入后，数组复制的下标相差1
                 }
-                tempSet[i + j] = mySet[i];
+                tempSet[i + j] = mySet[i];//如已成功插入，则会覆盖刚才添加到最后的e
             }
             mySet = tempSet;
         }
@@ -117,6 +118,13 @@ namespace MyGeneric
         {
             mySet = new T[0];
         }
+        //public IEnumerable<T> GetEnumerator()
+        //{
+        //    for (int index = 0; index < mySet.Length; index++)
+        //    {
+        //        yield return mySet[index];
+        //    }
+        //}
         public void Remove(T e)
         {
             int ei = -1;
@@ -136,8 +144,5 @@ namespace MyGeneric
             }
             mySet = tempSet;
         }
-        //public static bool operator >(T a, T b) => a.CompareTo(b) > 0;
-
-        //public static bool operator <(T a, T b) => a.CompareTo(b) < 0;
     }
 }
---
+++ Sample/LambdaExpression.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LambdaExpression1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda表达式的演变过程
+            // 下面是C# 1中创建委托实例的代码
+            Func<string, int> delegatetest1 = new Func<string, int>(Callbackmethod);
+
+            // C# 2中用匿名方法来创建委托实例，此时就不需要额外定义回调方法Callbackmethod
+            Func<string, int> delegatetest2 = delegate(string text)
+            {
+                return text.Length;
+            };
+            // C# 3中使用Lambda表达式来创建委托实例
+            Func<string, int> delegatetest3 = (string text) => text.Length;
+
+            // 可以省略参数类型string,把上面代码再简化为：
+            Func<string, int> delegatetest4 = (text) => text.Length;
+
+            // 此时可以把圆括号也省略,简化为：
+            Func<string, int> delegatetest = text => text.Length;
+
+            // 调用委托
+            Console.WriteLine("使用Lambda表达式返回字符串的长度为： " + delegatetest("learning hard"));
+            Console.Read();
+        }
+
+        // 回调方法
+        private static int Callbackmethod(string text)
+        {
+            return text.Length;
+        }
+    }
+}
+
+namespace LambdaExpression2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 新建一个button实例
+            Button button1 = new Button() {Text = "点击我"};
+
+            // C# 2中使用匿名方法来订阅事件
+            button1.Click += delegate(object sender, EventArgs e)
+            {
+                ReportEvent("Click事件", sender, e);
+            };
+            button1.KeyPress += delegate(object sender, KeyPressEventArgs e)
+            {
+                ReportEvent("KeyPress事件，即键盘按下事件", sender, e);
+            };
+
+            // C# 3Lambda表达式方式来订阅事件
+            button1.Click += (sender, e) => ReportEvent("Click事件", sender, e);
+            button1.KeyPress += (sender, e) => ReportEvent("KeyPress事件，即键盘按下事件", sender, e);
+
+            // C# 3中使用对象初始化器
+            Form form = new Form { Name = "在控制台中创建的窗体", AutoSize = true, Controls = { button1 } };
+
+            // 运行窗体
+            Application.Run(form);
+        }
+
+        // 记录事件的回调方法
+        private static void ReportEvent(string title, object sender, EventArgs e)
+        {
+            Console.WriteLine("发生的事件为：{0}", title);
+            Console.WriteLine("发生事件的对象为：{0}", sender);
+            Console.WriteLine("发生事件参数为： {0}", e.GetType());
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+}
