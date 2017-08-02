306ed2375feb76d10274f297d94922ec1b073fd2
谭九鼎 <109224573@qq.com>
2017-08-02 11:28:58 +0800

    更新实例

--- MyGeneric/MyGeneric/Program.cs
+++ MyGeneric/MyGeneric/Program.cs
@@ -35,8 +35,8 @@ namespace MyGeneric
 
     class MyStack<T>
     {
-        readonly T[] stack;
-        int top = -1;
+        readonly T[] _stack;
+        int _top = -1;
 
         public MyStack(int capacity = 256)
         {
@@ -45,40 +45,40 @@ namespace MyGeneric
             if (capacity * System.Runtime.InteropServices.Marshal.SizeOf<T>() > 8 * 1024 * 8)
                 throw new Exception("申请的栈太大！");
             Capacity = capacity;
-            stack = new T[capacity];
+            _stack = new T[capacity];
         }
 
         public int Capacity { get; private set; } // 最大容量
-        public int Count { get => top + 1; } // 当前个数
-        public void Clear() => top = -1;
+        public int Count { get => _top + 1; } // 当前个数
+        public void Clear() => _top = -1;
         public T Peek()
         {
-            if (top == -1)
+            if (_top == -1)
                 throw new Exception("栈为空！");
 
-            return stack[top];
+            return _stack[_top];
         }
         public T Pop()
         {
-            if (top < 0)
+            if (_top < 0)
                 throw new Exception("栈为空！");
 
-            return stack[top--];
+            return _stack[_top--];
         }
         public void Push(T e)
         {
             if (e == null)
                 throw new NullReferenceException();
-            if (top == Capacity - 1)
+            if (_top == Capacity - 1)
                 throw new Exception("栈已满！");
 
-            stack[++top] = e;
+            _stack[++_top] = e;
         }
     }
-    class MySortedSet<T> :IEnumerable<T> where T : IComparable // 有序不重复集合
+    class MySortedSet<T> :IEnumerable<T> where T : IComparable<T> // 有序不重复集合
     {
-        T[] mySet = new T[0];
-        public void Clear() => mySet = new T[0];
+        T[] _mySet = new T[0];
+        public void Clear() => _mySet = new T[0];
         public MySortedSet(int size = 256)
         {
             if (size < 0)
@@ -90,16 +90,16 @@ namespace MyGeneric
         IEnumerator IEnumerable.GetEnumerator()
         {
             for (int i = 0; i < Count; i++)
-                yield return mySet[i];
+                yield return _mySet[i];
         }
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             for (int i = 0; i < Count; i++)
-                yield return mySet[i];
+                yield return _mySet[i];
         }
-        public int Count { get => mySet.Length; }
+        public int Count { get => _mySet.Length; }
         public int MaxSize { get; private set; }
-        public T this [int index] { get => mySet[index]; }
+        public T this [int index] { get => _mySet[index]; }
 
         public void Add(T item)
         {
@@ -107,7 +107,7 @@ namespace MyGeneric
                 throw new NullReferenceException();
             if (Count == MaxSize)
                 throw new Exception("集合已满！");
-            foreach(T e in mySet)
+            foreach(T e in _mySet)
             if (e.Equals(item))
                 throw new Exception("给定的元素已经存在！");
 
@@ -115,14 +115,14 @@ namespace MyGeneric
             tempSet[Count] = item; // 直接把元素添加到最后一个
             for (int i = 0, j = 0; i < Count - 1; i++)
             {
-                if (item.CompareTo(mySet[i]) < 0)
+                if (item.CompareTo(_mySet[i]) < 0)
                 {
                     tempSet[i] = item;
                     j = 1; //成功插入后，数组复制的下标相差1
                 }
-                tempSet[i + j] = mySet[i]; //如已成功插入，则会覆盖刚才添加到最后的e
+                tempSet[i + j] = _mySet[i]; //如已成功插入，则会覆盖刚才添加到最后的e
             }
-            mySet = tempSet;
+            _mySet = tempSet;
         }
         public void Remove(T item)
         {
@@ -133,7 +133,7 @@ namespace MyGeneric
 
             int itemi = -1;
             for (int i = 0; i < Count - 1; i++)
-                if (mySet[i].Equals(item)) { itemi = i; break; }
+                if (_mySet[i].Equals(item)) { itemi = i; break; }
             if (itemi == -1)
                 throw new Exception("找不到给定的元素！");
 
@@ -142,9 +142,9 @@ namespace MyGeneric
             {
                 if (i == itemi)
                     j = 1;
-                tempSet[i] = mySet[i + j];
+                tempSet[i] = _mySet[i + j];
             }
-            mySet = tempSet;
+            _mySet = tempSet;
         }
     }
 }
---
+++ Sample/UserConversion.cs
@@ -0,0 +1,29 @@
+﻿// Illustrated C# 2012
+namespace UserConversion
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+
+        public static implicit operator int(Person p) // Person到int显式转换
+        {
+            return p.Age;
+        }
+    }
+
+    class Employee : Person { } // Employee到Person有隐式转换
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee bill = new Employee();
+            bill.Name = "William";
+            bill.Age = 25;
+
+            float fVar = bill; // int到float隐式转换
+            System.Console.WriteLine($"Person Info: {bill.Name}, {fVar}");
+        }
+    }
+}
