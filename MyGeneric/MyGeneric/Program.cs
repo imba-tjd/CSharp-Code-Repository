ec689e62db2097791d380ae00c4485a1f00ef0ce
谭九鼎 <109224573@qq.com>
2017-08-23 16:15:55 +0800

    日常更新

--- MyGeneric/MyGeneric/Program.cs
+++ MyGeneric/MyGeneric/Program.cs
@@ -33,58 +33,64 @@ namespace MyGeneric
         }
     }
 
-    class MyStack<T>
+    class MyStack<T> : IEnumerable<T>, IReadOnlyCollection<T>
     {
         readonly T[] _stack;
         int _top = -1;
+        bool IsFull => _top == Capacity - 1;
+        bool IsEmpty => _top < 0;
 
         public MyStack(int capacity = 256)
         {
             if (capacity < 0)
-                throw new Exception("栈大小不能为负！");
-            if (capacity * System.Runtime.InteropServices.Marshal.SizeOf<T>() > 8 * 1024 * 8)
-                throw new Exception("申请的栈太大！");
+                throw new ArgumentOutOfRangeException(nameof(capacity), "栈大小不能为负！");
+            if (capacity * System.Runtime.InteropServices.Marshal.SizeOf<T>() > 8 * 1024 * 4)
+                throw new ArgumentOutOfRangeException(nameof(capacity), "申请的栈太大！");
             Capacity = capacity;
             _stack = new T[capacity];
         }
 
-        public int Capacity { get; private set; } // 最大容量
-        public int Count { get => _top + 1; } // 当前个数
+        public int Capacity { get; } // 最大容量
+        public int Count => _top + 1; // 当前个数
         public void Clear() => _top = -1;
+
+        public IEnumerator<T> GetEnumerator() => _stack.GetEnumerator() as IEnumerator<T>;
+        IEnumerator IEnumerable.GetEnumerator() => _stack.GetEnumerator();
+
         public T Peek()
         {
-            if (_top == -1)
-                throw new Exception("栈为空！");
+            if (IsEmpty)
+                throw new InvalidOperationException("栈为空！");
 
             return _stack[_top];
         }
         public T Pop()
         {
-            if (_top < 0)
-                throw new Exception("栈为空！");
+            if (IsEmpty)
+                throw new InvalidOperationException("栈为空！");
 
             return _stack[_top--];
         }
         public void Push(T e)
         {
-            if (e == null)
-                throw new NullReferenceException();
-            if (_top == Capacity - 1)
-                throw new Exception("栈已满！");
+            if (IsFull)
+                throw new StackOverflowException("栈已满！");
 
             _stack[++_top] = e;
         }
+
     }
-    class MySortedSet<T> :IEnumerable<T> where T : IComparable<T> // 有序不重复集合
+
+    class MySortedSet<T> : IEnumerable<T> where T : IComparable<T> // 有序集合
     {
         T[] _mySet = new T[0];
         public void Clear() => _mySet = new T[0];
         public MySortedSet(int size = 256)
         {
             if (size < 0)
-                throw new Exception("集合大小不能为负！");
-            if (size > 4096)
-                throw new Exception("申请的集合太大！");
+                throw new ArgumentOutOfRangeException("集合大小不能为负！");
+            if (size > 512)
+                throw new ArgumentOutOfRangeException("申请的集合太大！");
             MaxSize = size;
         }
         IEnumerator IEnumerable.GetEnumerator()
@@ -97,19 +103,24 @@ namespace MyGeneric
             for (int i = 0; i < Count; i++)
                 yield return _mySet[i];
         }
-        public int Count { get => _mySet.Length; }
+        public int Count => _mySet.Length;
         public int MaxSize { get; private set; }
-        public T this [int index] { get => _mySet[index]; }
+        public T this[int index] => _mySet[index];
 
-        public void Add(T item)
+        public bool Contains(T item)
         {
-            if (item == null)
-                throw new NullReferenceException();
+            foreach (T e in _mySet)
+                if (e.Equals(item))
+                    return true;
+            return false;
+        }
+
+        public bool Add(T item)
+        {
             if (Count == MaxSize)
-                throw new Exception("集合已满！");
-            foreach(T e in _mySet)
-            if (e.Equals(item))
-                throw new Exception("给定的元素已经存在！");
+                throw new InvalidOperationException("集合已满！");
+            if (Contains(item))
+                return false;
 
             T[] tempSet = new T[Count + 1];
             tempSet[Count] = item; // 直接把元素添加到最后一个
@@ -123,19 +134,20 @@ namespace MyGeneric
                 tempSet[i + j] = _mySet[i]; //如已成功插入，则会覆盖刚才添加到最后的e
             }
             _mySet = tempSet;
+            return true;
         }
-        public void Remove(T item)
+        public bool Remove(T item)
         {
             if (item == null)
-                throw new NullReferenceException();
+                return true;
             if (Count == 0)
-                throw new Exception("集合为空！");
+                throw new InvalidOperationException("集合为空！");
 
             int itemi = -1;
             for (int i = 0; i < Count - 1; i++)
                 if (_mySet[i].Equals(item)) { itemi = i; break; }
             if (itemi == -1)
-                throw new Exception("找不到给定的元素！");
+                return false;
 
             T[] tempSet = new T[Count - 1];
             for (int i = 0, j = 0; i < Count - 1; i++)
@@ -145,6 +157,7 @@ namespace MyGeneric
                 tempSet[i] = _mySet[i + j];
             }
             _mySet = tempSet;
+            return true;
         }
     }
 }
