176da9fe003c8fae11f2263b95f6e3bc826dc35d
谭九鼎 <109224573@qq.com>
2017-10-10 13:05:29 +0800

    原创小程序

---
+++ Sample/AddDomainsToPac.cs
@@ -0,0 +1,36 @@
+// 用于给ss的pac.txt添加域名
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace AddDomainsToPac
+{
+    class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        File.Copy("pac.txt", "pac.bak", true);
+        TextReader tr = new StreamReader("pac.txt");
+        StringBuilder sb = new StringBuilder();
+
+        if (args.Length == 0)
+        {
+            args = new string[1];
+            args[0] = Console.ReadLine();
+        }
+
+        sb.AppendLine(tr.ReadLine());
+        foreach (var arg in args)
+            sb.AppendLine(string.Format("  \"{0}\": 1,", arg).TrimEnd());
+        sb.Append(tr.ReadToEnd());
+
+        tr.Dispose();
+        TextWriter tw = new StreamWriter("pac.txt", false);
+        tw.Write(sb);
+        tw.Flush();
+        tw.Dispose();
+        System.Diagnostics.Process.Start("pac.txt");
+    }
+}
