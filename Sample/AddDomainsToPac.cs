ef576cd8ea5003088f6f420367ddabe8ce9fcaad
谭九鼎 <109224573@qq.com>
2017-11-03 17:07:52 +0800

    加上回花括号

--- Sample/AddDomainsToPac.cs
+++ Sample/AddDomainsToPac.cs
@@ -7,30 +7,31 @@ using System.Text;
 namespace AddDomainsToPac
 {
     class Program
-{
-    [STAThread]
-    static void Main(string[] args)
     {
-        File.Copy("pac.txt", "pac.bak", true);
-        TextReader tr = new StreamReader("pac.txt");
-        StringBuilder sb = new StringBuilder();
-
-        if (args.Length == 0)
+        [STAThread]
+        static void Main(string[] args)
         {
-            args = new string[1];
-            args[0] = Console.ReadLine();
+            File.Copy("pac.txt", "pac.bak", true);
+            TextReader tr = new StreamReader("pac.txt");
+            StringBuilder sb = new StringBuilder();
+
+            if (args.Length == 0)
+            {
+                args = new string[1];
+                args[0] = Console.ReadLine();
+            }
+
+            sb.AppendLine(tr.ReadLine());
+            foreach (var arg in args)
+                sb.AppendLine(string.Format("  \"{0}\": 1,", arg.Trim().TrimStart()));
+            sb.Append(tr.ReadToEnd());
+
+            tr.Dispose();
+            TextWriter tw = new StreamWriter("pac.txt", false);
+            tw.Write(sb);
+            tw.Flush();
+            tw.Dispose();
+            System.Diagnostics.Process.Start("pac.txt");
         }
-
-        sb.AppendLine(tr.ReadLine());
-        foreach (var arg in args)
-            sb.AppendLine(string.Format("  \"{0}\": 1,", arg.Trim()));
-        sb.Append(tr.ReadToEnd());
-
-        tr.Dispose();
-        TextWriter tw = new StreamWriter("pac.txt", false);
-        tw.Write(sb);
-        tw.Flush();
-        tw.Dispose();
-        System.Diagnostics.Process.Start("pac.txt");
     }
 }
