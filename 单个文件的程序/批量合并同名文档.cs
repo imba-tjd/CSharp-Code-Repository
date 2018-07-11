ca3a427f643419b5577f84ef352f6f9e89bb0494
imba-tjd <109224573@qq.com>
2018-07-11 18:34:57 +0800

    Upload 批量合并同名文档

---
+++ 单个文件的程序/批量合并同名文档.cs
@@ -0,0 +1,35 @@
+﻿// https://www.zhihu.com/question/284015230
+using System;
+// using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Dotnet1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Directory.GetFiles(
+                Environment.CurrentDirectory, "*.txt", SearchOption.AllDirectories
+                ).GroupBy(
+                    x => Path.GetFileName(x)
+                    ).AsParallel().ForAll(
+                        x =>
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            foreach (var item in x)
+                                using (TextReader tr = new StreamReader(item))
+                                {
+                                    sb.AppendLine(tr.ReadToEnd() + Environment.NewLine);
+                                }
+                            using (TextWriter tw = new StreamWriter(x.Key))
+                            {
+                                tw.Write(sb.ToString());
+                            }
+                        }
+                        );
+        }
+    }
+}
