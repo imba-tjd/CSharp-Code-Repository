e86fcbf41337db7905ee3665aa08d1b2261b59bd
imba-tjd <109224573@qq.com>
2018-07-13 18:48:12 +0800

    Update 批量合并同名文档：format code && fix IndexOutOfRangeException

--- 单个文件的程序/批量合并同名文档.cs
+++ 单个文件的程序/批量合并同名文档.cs
@@ -12,28 +12,26 @@ namespace DotNet
         static void Main(string[] args)
         {
             Directory.GetFiles(
-                args?[1] ?? Environment.CurrentDirectory, "*.txt", SearchOption.AllDirectories
-                ).GroupBy(
-                    x => Path.GetFileName(x)
-                    ).AsParallel().ForAll(
-                        x =>
-                        {
-                            StringBuilder sb = new StringBuilder();
-                            foreach (var item in x)
-                                sb.AppendLine(File.ReadAllText(item) + Environment.NewLine);
-                            File.WriteAllText(x.Key, sb.ToString());
+                args.Length > 0 ? args[1] : Environment.CurrentDirectory, "*.txt", SearchOption.AllDirectories
+                ).GroupBy(x => Path.GetFileName(x)
+                    ).AsParallel().ForAll(x =>
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        foreach (var item in x)
+                            sb.AppendLine(File.ReadAllText(item) + Environment.NewLine);
+                        File.WriteAllText(x.Key, sb.ToString());
 
-                            // foreach (var item in x)
-                            //     using (TextReader tr = new StreamReader(item))
-                            //     {
-                            //         sb.AppendLine(tr.ReadToEnd());
-                            //         sb.AppendLine();
-                            //     }
-                            // using (TextWriter tw = new StreamWriter(x.Key))
-                            // {
-                            //     tw.Write(sb.ToString());
-                            // }
-                        }
+                        // foreach (var item in x)
+                        //     using (TextReader tr = new StreamReader(item))
+                        //     {
+                        //         sb.AppendLine(tr.ReadToEnd());
+                        //         sb.AppendLine();
+                        //     }
+                        // using (TextWriter tw = new StreamWriter(x.Key))
+                        // {
+                        //     tw.Write(sb.ToString());
+                        // }
+                    }
                         );
         }
     }
